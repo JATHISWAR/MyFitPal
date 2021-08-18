e922ca0ddbd04387de8c8ee4637b57ab7db980cc
jathiswarbhaskar <codguy2802@gmail.com>
2021-08-18 11:49:36 +0530

    SplashScreen

---
+++ app/src/main/java/com/jathiswarbhaskar/myfitpal/SplashScreenActivity.java
@@ -0,0 +1,27 @@
+package com.jathiswarbhaskar.myfitpal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private int SPLASH_SCREEN_LENGTH = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        },SPLASH_SCREEN_LENGTH);
+    }
+}
