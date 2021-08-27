38ba94e659898469ac1713d5c2bf958e10b3ef52
jathiswarbhaskar <codguy2802@gmail.com>
2021-08-28 00:25:28 +0530

    Fragments

---
+++ app/src/main/java/com/jathiswarbhaskar/myfitpal/HomeActivity.java
@@ -0,0 +1,52 @@
+package com.jathiswarbhaskar.myfitpal;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navlistener);
+        if(savedInstanceState==null){
+            bottomNav.setSelectedItemId(R.id.nav_home);
+        }
+
+
+    }
+
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navlistener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+            switch (item.getItemId()){
+                case R.id.nav_home:
+                    selectedFragment = new HomeFragment();
+                    break;
+                case R.id.nav_food:
+                    selectedFragment = new FoodFragment();
+                    break;
+                case R.id.nav_run:
+                    selectedFragment = new RunFragment();
+                    break;
+
+            }
+
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+            return true;
+        }
+    };
+
+}
--- app/src/main/java/com/jathiswarbhaskar/myfitpal/MainActivity.java
+++ app/src/main/java/com/jathiswarbhaskar/myfitpal/MainActivity.java
@@ -1,16 +1,120 @@
 package com.jathiswarbhaskar.myfitpal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.MenuItem;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button loginbtn, signupbtn;
+    private EditText memail, mpassword;
+    private FirebaseAuth mAuth;
+    private ProgressDialog loadingBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        memail = (EditText) findViewById(R.id.login_email_input);
+        mpassword = (EditText) findViewById(R.id.login_password_input);
+        loginbtn = (Button) findViewById(R.id.login_button);
+        mAuth = FirebaseAuth.getInstance();
+        loadingBar = new ProgressDialog(this);
+        Login();
+
+
     }
+
+
+    public void Login() {
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = memail.getText().toString().trim();
+                String password = mpassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    memail.setError("Email is required");
+                    return;
+                } else if (TextUtils.isEmpty(password)) {
+                    mpassword.setError("Password is required");
+                    return;
+                } else if (TextUtils.isEmpty(password) && TextUtils.isEmpty(email)) {
+                    memail.setError("Email is required");
+                    mpassword.setError("Password is required");
+                    return;
+                } else {
+                    loadingBar.setTitle("Logging In");
+                    loadingBar.setMessage("Please Wait while we are checking the credentials...");
+                    loadingBar.setCanceledOnTouchOutside(false);
+                    loadingBar.show();
+                }
+
+                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                            startActivity(intent);
+                        } else {
+                            loadingBar.dismiss();
+                            AlertDialog.Builder builder1 = new AlertDialog.Builder(MainActivity.this);
+                            builder1.setMessage("Please Check Your User Id and Password.");
+                            builder1.setCancelable(true);
+
+                            builder1.setPositiveButton(
+                                    "Ok",
+                                    new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dialog, int id) {
+                                            dialog.cancel();
+                                        }
+                                    });
+                            AlertDialog alert11 = builder1.create();
+                            alert11.show();
+
+                            mpassword.getText().clear();
+                            memail.getText().clear();
+
+                        }
+                    }
+                });
+
+
+            }
+        });
+
+
+    }
+
+
+
+
+
+
+
+
+
 }
