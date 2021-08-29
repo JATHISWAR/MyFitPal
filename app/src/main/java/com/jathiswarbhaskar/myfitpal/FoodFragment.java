360b4b0e0ce2ce0e4e5a5ab28775a4853bb6e95c
jathiswarbhaskar <codguy2802@gmail.com>
2021-08-29 11:38:07 +0530

    NavBar

--- app/src/main/java/com/jathiswarbhaskar/myfitpal/FoodFragment.java
+++ app/src/main/java/com/jathiswarbhaskar/myfitpal/FoodFragment.java
@@ -4,15 +4,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class FoodFragment extends Fragment {
+    private View foodFragmentView;
+    private Button button;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_food,container,false);
+        foodFragmentView = inflater.inflate(R.layout.fragment_food,container,false);
+
+
+
+        return foodFragmentView;
     }
 }
